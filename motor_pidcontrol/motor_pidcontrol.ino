cb50129886f12b0b9ed6c2e80baaf62c55ff1116
gongminsu-maker <gongminsu@naver.com>
2025-03-09 21:45:44 +0900

    isitfinal?

--- motor_pidcontrol/motor_pidcontrol.ino
+++ motor_pidcontrol/motor_pidcontrol.ino
@@ -7,53 +7,55 @@
 //motor 관련부
 #include <geometry_msgs/msg/twist.h>
 #include <my_custom_message/msg/motor.h>
+//imu센서 관련부
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <sensor_msgs/msg/imu.h>
 
 geometry_msgs__msg__Twist msg;
 my_custom_message__msg__Motor motor;
 rcl_subscription_t subscriber;
+sensor_msgs__msg__Imu imu;
 rclc_executor_t executor;
 rcl_allocator_t allocator;
 rclc_support_t support;
-rcl_publisher_t encoder_publisher;
+rcl_publisher_t encoder_publisher, imu_publisher; 
 rcl_node_t node;
 rcl_timer_t timer;
 
+#define LEFT_MOTOR_PWM  19   // 왼쪽 모터 PWM 핀
+#define LEFT_MOTOR_DIR1 18  // 왼쪽 모터 방향 핀 1
+#define LEFT_MOTOR_DIR2 5  // 왼쪽 모터 방향 핀 2
 
-// 모터 및 엔코더 핀 정의
-// left
-#define LEFT_MOTOR_PWM  19
-#define LEFT_MOTOR_DIR1 18
-#define LEFT_MOTOR_DIR2 5
-#define LEFT_ENCODER_A  39
-#define LEFT_ENCODER_B  36
-// right
-#define RIGHT_MOTOR_PWM 4 
-#define RIGHT_MOTOR_DIR1 16 
-#define RIGHT_MOTOR_DIR2 17
-#define RIGHT_ENCODER_A 35 
-#define RIGHT_ENCODER_B 34
+#define RIGHT_MOTOR_PWM 4  // 오른쪽 모터 PWM 핀
+#define RIGHT_MOTOR_DIR1 16 // 오른쪽 모터 방향 핀 1
+#define RIGHT_MOTOR_DIR2 17 // 오른쪽 모터 방향 핀 2
 
+#define LEFT_ENCODER_A 39  // 왼쪽 엔코더 A 채널
+#define LEFT_ENCODER_B 36  // 왼쪽 엔코더 B 채널
+#define RIGHT_ENCODER_A 35 // 오른쪽 엔코더 A 채널
+#define RIGHT_ENCODER_B 34 // 오른쪽 엔코더 B 채널
 
-// **모터 및 엔코더 상수**
-#define PPR 11
-#define GEAR_RATIO 90
+#define WHEEL_SEPARATION 0.261  // 바퀴 간 거리 (30cm)
+#define PPR 11                  // 엔코더 펄스
+#define GEAR_RATIO 90           // 감속비
 #define ENCODER_FACTOR (2.0 * M_PI / (PPR * GEAR_RATIO * 2)) // 각속도 변환 계수
-#define D 0.0875 // 휠의 지름 (m)
 #define R 0.04375 // 휠의 반지름 (m)
-#define WHEEL_SEPARATION 0.261 
-#define MAX_MOTOR_RPM 110  // 모터 최대 RPM
 #define MAX_PWM 255 // 9v = 255, 12v = 240
 #define MIN_PWM 0  // 모터가 동작하는 최소 PWM 값
 
+#define EARTH_GRAVITY_MS2 9.80665  // m/s²
+#define DEG_TO_RAD 0.017453292519943295769236907684886 //imu rad/s변환상수
+
 // PID 제어 변수
 //12V
-float Kp_L = 5.0;  
-float Ki_L = 5.0;   
-float Kd_L = 0.05;  
+float Kp_L = 4.0;  
+float Ki_L = 10.0;   
+float Kd_L = 0.0;  
 
-float Kp_R = 5.0;  
-float Ki_R = 5.0;   
-float Kd_R = 0.05;  
+float Kp_R = 3.5; 
+float Ki_R = 10.0;   
+float Kd_R = 0.7;  
 //목표값, 에러 변수
 float desired_left_speed = 0.0;
 float desired_right_speed = 0.0;
@@ -67,7 +69,6 @@ float error_right = 0.0, last_error_right = 0.0, right_integral = 0.0;
 float control_output_left = 0.0;
 float control_output_right = 0.0;
 int motor_pwm = 0;
-
 // 엔코더 측정 변수
 volatile int left_encoder_count = 0;
 volatile int right_encoder_count = 0;
@@ -77,15 +78,31 @@ unsigned long last_time_right = 0;
 float left_offset_count = 0.0;
 float right_offset_count = 0.0;
 //LPF 변수
-float alpha = 0.25;  // 필터 계수 
+float alpha = 0.4;  // 필터 계수 
 float LPF_target_omega_left = 0.0;
 float LPF_target_omega_right = 0.0;
+// 미분항 LPF
+float alpha_d = 0.7;  // D항 LPF 계수 
+float filtered_derivative_left = 0.0;
+float filtered_derivative_right = 0.0;
+
+
+//imu 관련 변수 선언
+
+MPU6050 mpu; 
+Quaternion q;
+VectorInt16 aa;       
+VectorInt16 gg; 
+bool DMPReady = false;
+uint8_t devStatus;
+uint16_t packetSize;
+uint8_t FIFOBuffer[64];
+
 
 
 // 에러 매크로
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-// offset
 
 // 오류 발생 시 루프
 void error_loop(){
@@ -118,7 +135,6 @@ void offset() {
     Serial.print(" Right Encoder Offset: ");
     Serial.println(right_offset_count);
 }
-
 // ROS 2에서 `/cmd_vel` 메시지 수신 시 실행되는 콜백 함수
 void subscription_callback(const void *msgin) {
     const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
@@ -129,8 +145,8 @@ void subscription_callback(const void *msgin) {
     if (abs(desired_speed_mps) < 0.01) desired_speed_mps = 0.0;
     if (abs(order_w) < 0.01) order_w = 0.0;
     // 목표 각속도 계산 (rad/s)
-    desired_left_speed = desired_speed_mps + (order_w*WHEEL_SEPARATION)/2;
-    desired_right_speed = desired_speed_mps - (order_w*WHEEL_SEPARATION)/2;
+    desired_left_speed = desired_speed_mps - (order_w*WHEEL_SEPARATION)/2;
+    desired_right_speed = desired_speed_mps + (order_w*WHEEL_SEPARATION)/2;
 
     target_omega_left = desired_left_speed / R;
     target_omega_right = desired_right_speed / R;
@@ -189,7 +205,6 @@ void rightsetMotorPWM(int pwm) {
     Serial.println(abs(pwm));
     analogWrite(RIGHT_MOTOR_PWM, abs(pwm));
 }
-
 // 속도 측정 및 PID 제어 실행
 // left
 void updateleftControlLoop() {
@@ -203,20 +218,23 @@ void updateleftControlLoop() {
 
     // 현재 속도 측정 (rad/s)
     measured_w_left = ((left_encoder_count-left_offset_count) * ENCODER_FACTOR) / dt;
+    if (abs(measured_w_left) >= 0.0 && abs(measured_w_left) < 0.05) { //노이즈 무시
+        measured_w_left = 0.0;
+    }
     left_encoder_count = 0; // 카운트 리셋
     LPF_target_omega_left = alpha*target_omega_left + (1-alpha)*LPF_target_omega_left;    
     //  오차 계산
     error_left = LPF_target_omega_left - measured_w_left;
+
     Serial.print("target_left");
-    Serial.println(target_omega_left);
+    Serial.println(LPF_target_omega_left);
     Serial.print("error_left");
     Serial.println(error_left);
     Serial.print("measured_left");
     Serial.println(measured_w_left);
-
-    
     // PID 계산
-    if (abs(error_left) < 1.0){
+
+    if (abs(error_left) < 1.5){
       left_integral += error_left * dt;
     } else if (abs(error_left < 0.05)) {
       left_integral = 0.0;
@@ -225,9 +243,12 @@ void updateleftControlLoop() {
     } else {
       left_integral = 0.0;
     }
-
-    float derivative = (error_left - last_error_left) / dt;
-    control_output_left = (Kp_L * error_left) + (Ki_L * left_integral) + (Kd_L * derivative);
+    // 미분을 dt로 표현
+    //float derivative = (error_left - last_error_left) / dt;
+    // 미분을 LPF로 표현
+    float derivative_raw = (error_left - last_error_left);
+    filtered_derivative_left = alpha_d * filtered_derivative_left + (1 - alpha_d) * derivative_raw;
+    control_output_left = (Kp_L * error_left) + (Ki_L * left_integral) + (Kd_L * filtered_derivative_left);
     Serial.print("pid_output_L");
     Serial.println(control_output_left);
     
@@ -239,7 +260,10 @@ void updateleftControlLoop() {
     //Serial.println(control_output_left);
 
     // PWM 변환 (새로운 공식 사용)
-    float left_pwm = 2.4425*control_output_left*control_output_left - 2.6619*control_output_left + 46.902 ; 
+    //무부하 mapping
+    //float left_pwm = 2.4425*control_output_left*control_output_left - 2.6619*control_output_left + 46.902 ; 
+    //지면 주행 mapping
+    float left_pwm = 1.4111*control_output_left*control_output_left + 6.4437*control_output_left + 50.688 ; 
     int motor_pwm = round(left_pwm);
     // 모터에 PWM 적용
     leftsetMotorPWM(motor_pwm);
@@ -256,6 +280,9 @@ void updaterightControlLoop() {
 
     // 1. 현재 속도 측정 (rad/s)
     measured_w_right = ((right_encoder_count-right_offset_count) * ENCODER_FACTOR) / dt;
+    if (abs(measured_w_right) >= 0.0 && abs(measured_w_right) < 0.05) { //노이즈 무시
+        measured_w_right = 0.0;
+    }
     right_encoder_count = 0; // 카운트 리셋
 
     // 2. 오차 계산
@@ -269,7 +296,7 @@ void updaterightControlLoop() {
     Serial.println(measured_w_right);
     
     // 3. PID 계산
-    if (abs(error_right)< 1.0) {
+    if (abs(error_right)< 1.5) {
       right_integral += error_right*dt;
     } else if (abs(error_right)<0.05) {
       right_integral = 0.0;
@@ -278,9 +305,13 @@ void updaterightControlLoop() {
     } else {
       right_integral = 0.0;
     }
-  
-    float derivative = (error_right - last_error_right) / dt;
-    control_output_right = (Kp_R * error_right) + (Ki_R * right_integral) + (Kd_R * derivative);
+    // 미분을 dt로 표현
+    //float derivative = (error_right - last_error_right) / dt;
+    // 미분을 LPF로 표현
+    float derivative_raw = (error_left - last_error_right);
+    filtered_derivative_right = alpha_d * filtered_derivative_right + (1 - alpha_d) * derivative_raw;
+
+    control_output_right = (Kp_R * error_right) + (Ki_R * right_integral) + (Kd_R * filtered_derivative_right);
     Serial.print("pid_output_R");
     Serial.println(control_output_right);
     
@@ -288,7 +319,9 @@ void updaterightControlLoop() {
     last_time_right = current_time;
 
     // 4. PWM 변환 
-    float right_pwm = 2.3216*control_output_right*control_output_right - 3.1183*control_output_right + 46.695 ; 
+    // 무부하 mapping
+    //float right_pwm = 2.3216*control_output_right*control_output_right - 3.1183*control_output_right + 46.695 ; 
+    float right_pwm = 2.16*control_output_right*control_output_right + 0.7162*control_output_right + 52.622 ; 
     int motor_pwm = round(right_pwm);
 
     // 5. 모터에 PWM 적용
@@ -302,61 +335,131 @@ void update_motor_velocity() {
   motor.left_target_w = LPF_target_omega_left;
   motor.right_target_w = LPF_target_omega_right;
 }
-// 모터 속도, imu 정보 발행 콜백함수
+
+  // imu데이터를 읽어서 메시지에 저장
+void update_imu_data(){
+  if (!DMPReady) {
+    Serial.println("DMP not ready! Skipping IMU update.");
+    return;
+  }
+
+  if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
+    mpu.dmpGetAccel(&aa, FIFOBuffer);
+    mpu.dmpGetGyro(&gg, FIFOBuffer);
+    mpu.dmpGetQuaternion(&q, FIFOBuffer);
+    //센서 쿼터니언
+    imu.orientation.x = q.x;
+    imu.orientation.y = q.y;
+    imu.orientation.z = q.z;
+    imu.orientation.w = q.w;
+
+    // 가속도 데이터 변환 (m/s² 단위)
+    imu.linear_acceleration.x = aa.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
+    imu.linear_acceleration.y = -aa.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
+    // 자이로 데이터 변환 (rad/s 단위)
+    imu.angular_velocity.z = gg.z * mpu.get_gyro_resolution() * DEG_TO_RAD;
+
+    // ROS 2 메시지 헤더 업데이트
+    imu.header.stamp.sec = millis() / 1000;
+    imu.header.stamp.nanosec = (millis() % 1000) * 1000000;
+
+  }
+}
+
+// **모터 속도, imu 정보 발행 콜백함수**
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time){
   RCLC_UNUSED(last_call_time);
   if (timer != NULL){
     update_motor_velocity();
+    update_imu_data();
     RCSOFTCHECK(rcl_publish(&encoder_publisher,&motor,NULL));
+    RCSOFTCHECK(rcl_publish(&imu_publisher,&imu, NULL));
   }
 }
 
+// **ESP32 초기 설정**
 void setup() {
-    //set_microros_wifi_transports("HOTPOT","18123030", "192.168.98.124", 8888);
-    set_microros_wifi_transports("HY-DORM5-658","residence658", "192.168.0.8", 8888);
-    //set_microros_wifi_transports("KT_GiGA_5D35","ahb66kz314", "172.30.1.58", 8888);
-    //set_microros_wifi_transports("I_phone","dlgksrufdlek", "192.168.166.124", 8888);
-    Serial.begin(115200);
-    delay(2000);
+  set_microros_wifi_transports("HOTPOT","18123030", "192.168.25.124", 8888);
+  //set_microros_wifi_transports("HY-DORM5-658","residence658", "192.168.0.8", 8888);
+  //set_microros_wifi_transports("KT_GiGA_5D35","ahb66kz314", "172.30.1.58", 8888);
+  //set_microros_wifi_transports("I_phone","dlgksrufdlek", "192.168.166.124", 8888);
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(100000); //100kHZ로 설정
+  //imu 초기화 및 연결 확인코드
+  delay(2000);
+  Serial.println("initializing MPU6050");
+  mpu.initialize();
+  Serial.println("Testing MPU6050 connection...");
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 connection failed");
+    while (1);
+  }
+  Serial.println("MPU6050 connection successful");
+  Serial.println("Initializing DMP...");
+  devStatus = mpu.dmpInitialize();
+  // IMU 오프셋 설정
+  mpu.setXAccelOffset(0);
+  mpu.setYAccelOffset(0);
+  mpu.setZAccelOffset(0);
+  mpu.setXGyroOffset(0);
+  mpu.setYGyroOffset(0);
+  mpu.setZGyroOffset(0);
 
-    // 모터 핀 설정
-    pinMode(LEFT_MOTOR_PWM, OUTPUT);
-    pinMode(LEFT_MOTOR_DIR1, OUTPUT);
-    pinMode(LEFT_MOTOR_DIR2, OUTPUT);
-    pinMode(RIGHT_MOTOR_PWM, OUTPUT);
-    pinMode(RIGHT_MOTOR_DIR1, OUTPUT);
-    pinMode(RIGHT_MOTOR_DIR2, OUTPUT);
+  if (devStatus == 0) {
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    Serial.println("DMP ready!");
+    mpu.setDMPEnabled(true);
+    DMPReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    Serial.println("DMP initialization failed!");
+  }
+    // IMU 메시지 초기화 frame_id설정 동적 string 동적 메모리할당 size,capacity지정해 주어야함.(c언어임으로.)
+  imu.header.frame_id.data = (char *)"base_link";
+  imu.header.frame_id.size = strlen("base_link");
+  imu.header.frame_id.capacity = imu.header.frame_id.size + 1;
 
-    // 엔코더 핀 설정 및 인터럽트 활성화
-    pinMode(LEFT_ENCODER_A, INPUT);
-    pinMode(LEFT_ENCODER_B, INPUT);
-    pinMode(RIGHT_ENCODER_A, INPUT);
-    pinMode(RIGHT_ENCODER_B, INPUT);
-    attachInterrupt(LEFT_ENCODER_A, left_encoder_ISR, CHANGE);
-    attachInterrupt(RIGHT_ENCODER_A, right_encoder_ISR, CHANGE);
-    //offset();
-    Serial.println("offset done");
-     //micro_ros 설정 부분
-    allocator = rcl_get_default_allocator();
+  // 모터 핀을 출력 모드로 설정
+  pinMode(LEFT_MOTOR_PWM, OUTPUT);
+  pinMode(LEFT_MOTOR_DIR1, OUTPUT);
+  pinMode(LEFT_MOTOR_DIR2, OUTPUT);
+  pinMode(RIGHT_MOTOR_PWM, OUTPUT);
+  pinMode(RIGHT_MOTOR_DIR1, OUTPUT);
+  pinMode(RIGHT_MOTOR_DIR2, OUTPUT);
+  //엔코더 핀모드
+  pinMode(LEFT_ENCODER_A, INPUT);
+  pinMode(LEFT_ENCODER_B, INPUT);
+  pinMode(RIGHT_ENCODER_A, INPUT);
+  pinMode(RIGHT_ENCODER_B, INPUT);
+  //2체배 방식 채널 A의 rising, falling모두 인식
+  attachInterrupt(LEFT_ENCODER_A, left_encoder_ISR, CHANGE);
+  attachInterrupt(RIGHT_ENCODER_A, right_encoder_ISR, CHANGE);
+  //offset();
 
-    RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-    RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
-    RCCHECK(rclc_subscription_init_default(&subscriber,&node,ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),"/cmd_vel"));
-    //엔코더를 통한 속도값 퍼블리셔
-    RCCHECK(rclc_publisher_init_default(&encoder_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(my_custom_message, msg, Motor), "motor_topic"));
-    RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(50), timer_callback));
-    RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+  //micro_ros 설정 부분
+  allocator = rcl_get_default_allocator();
 
-    RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
-    RCCHECK(rclc_executor_add_timer(&executor, &timer));
-    //RCCHECK(rclc_executor_set_trigger(&executor,rclc_executor_trigger_one, &subscriber));
-    Serial.println("micro-ROS node started.");
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
+  RCCHECK(rclc_subscription_init_default(&subscriber,&node,ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),"/cmd_vel"));
+  //엔코더를 통한 속도값 퍼블리셔
+  RCCHECK(rclc_publisher_init_default(&encoder_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(my_custom_message, msg, Motor), "motor_topic"));
+  //imu데이터 퍼블리셔
+  RCCHECK(rclc_publisher_init_default(&imu_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs,msg,Imu), "/imu/data"));
+  RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(50), timer_callback));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
+
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  //RCCHECK(rclc_executor_set_trigger(&executor,rclc_executor_trigger_one, &subscriber));
+  Serial.println("micro-ROS node started.");
 }
-
 
 void loop() {
     RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(50)));
     updateleftControlLoop();
     updaterightControlLoop();
-    delay(50);
+    delay(20);
 }
